18a2beff3b960b7df125994b0484d9a6bb70d7ff
Ben Hall <ben@benhall.me.uk>
2015-01-06 14:19:52 +0000

    Initial setup. Varnish routing configured via environment variables

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for name in VARNISH_BACKEND_PORT VARNISH_BACKEND_HOST
+do
+    eval value=\$$name
+    sed -i "s|{${name}}|${value}|g" /etc/varnish/default.vcl
+done
+
+varnishd -f /etc/varnish/default.vcl -s malloc,100M -a 0.0.0.0:${VARNISH_PORT}
+varnishlog
