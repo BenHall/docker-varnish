349ea3e6b9fcc24916a26ebc8477e528ae893f59
Ben Hall <Ben@Benhall.me.uk>
2015-01-06 16:12:24 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for name in VARNISH_BACKEND_PORT VARNISH_BACKEND_HOST
+for name in VARNISH_BACKEND_PORT VARNISH_BACKEND_HOST VARNISH_BACKEND_DOMAIN
 do
     eval value=\$$name
     sed -i "s|{${name}}|${value}|g" /etc/varnish/default.vcl
